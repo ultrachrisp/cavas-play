fe62a0904a871fb82040cae0bc81d8d54e588ee0
Chrisp <ultrachrisp@gmail.com>
2019-08-06 13:29:59 +0200

    Trying out figwheel-main

--- src/canvas_play/core.cljs
+++ src/canvas_play/core.cljs
@@ -1,4 +1,17 @@
-(ns canvas_play.core)
+(ns ^:figwheel-hooks canvas-play.core
+  (:require
+   [goog.dom :as gdom]))
+
+(println "This text is printed from src/canvas_play/core.cljs. Go ahead and edit it and see reloading in action.")
+
+(defn multiply [a b] (* a b))
+
+
+;; define your app data so that it doesn't get over-written on reload
+;;(defonce app-state (atom {:text "Hello world!"}))
+
+(defn get-app-element []
+  (gdom/getElement "app"))
 
 (def display (.getElementById js/document "display"))
 (def context (.getContext display "2d"))
@@ -98,3 +111,10 @@
 
 (init-canvas)
 (render)
+
+;; specify reload hook with ^;after-load metadata
+(defn ^:after-load on-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
