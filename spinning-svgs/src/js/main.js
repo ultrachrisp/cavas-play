13ce1c205def2a0f7751f6e8b733c52e7796d36d
Chrisp <ultrachrisp@gmail.com>
2019-09-09 13:50:47 +0200

    Restructuring.

--- spinning-svgs/src/js/main.js
+++ spinning-svgs/src/js/main.js
@@ -1,14 +1,9 @@
-// const json = '[{"hello": "hazit"}]';
-
 export const tester = () => {return 77;};
 
 const getElement = selector => document.querySelector(selector).innerHTML;
 const getJson = elem => JSON.parse(elem);
 
-// const start =getJson(getElement("script[type='application/json']"));
-const start = str => str
-      |> getElement
-      |> getJson;
+const start = () => getJson(getElement("script[type='application/json']"));
 
 const tagID = start("script[type='application/json']");
 console.log(tagID);
