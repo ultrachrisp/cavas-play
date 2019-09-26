8615c3c2bbbc259f5e1553336f46963dfcc1e6fc
Chrisp <ultrachrisp@gmail.com>
2019-09-26 14:14:50 +0200

    Rebooting... this repo is in a sorry state.

---
+++ spinning-svgs/__test__/main.test.js
@@ -0,0 +1,6 @@
+// import { tester } from '../src/js/main.js';
+
+test('Default test', () => {
+    expect(22).toBe(22);
+});
+
--- spinning-svgs/src/js/main.js
+++ spinning-svgs/src/js/main.js
@@ -1,15 +1,11 @@
-export const tester = () => {return 77;};
+import Particle from './particle';
 
-const getElement = selector => document.querySelector(selector).innerHTML;
-const getJson = elem => JSON.parse(elem);
-
-const start = () => getJson(getElement("script[type='application/json']"));
-
-const tagID = start("script[type='application/json']");
-console.log(tagID);
-
-export function Main(){
-    return {
-        test: () => tester()
-    };
+const settings = {
+    tag: ".dashboard"
 };
+
+function init (){
+    document.querySelector(settings.tag).innerHTML = "";
+}
+
+init();
